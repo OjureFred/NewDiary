af859881eb3bc7872b132e1a731bd8f05e92a271
Fredrick Ojure <fredojure@hotmail.com>
2020-10-25 20:45:46 +0300

    Coded the Entry Detail component

--- src/app/entry.ts
+++ src/app/entry.ts
@@ -1,8 +1,9 @@
 export class Entry {
-    id: number;
-    name: string;
-    description: string;
-    days: number;
-    date: Date;
-    status: string;
+
+    showDetails: boolean;
+
+    constructor(public id: number, public name: string, public description: string, public days: number,
+        public date: Date, public status: string) {
+        this.showDetails = false;
+    }
 }
--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -10,13 +10,14 @@ import { Entry } from '../entry';
 export class EntryComponent implements OnInit {
 
   entries: Entry[] = [
-    { id: 1, name: 'Watch Tutorial', description: 'Watch Docker Tutorial', days: 3, date: new Date(2020, 10, 26), status: 'NS' },
-    { id: 2, name: 'DevelopApp', description: 'Develop Simple Angular Application', days: 4, date: new Date(2021, 1, 2), status: 'WIP' },
-    {
-      id: 3, name: 'Negotiate Project', description: 'Finalize Hours and get project go-ahead from Charles', days: 1,
-      date: new Date(2020, 11, 12), status: 'DONE'
-    }
+    new Entry(1, 'Watch Tutorial','Watch Docker Tutorial', 3, new Date(2020, 10, 26), 'NS'),
+    new Entry(2, 'DevelopApp', 'Develop Simple Angular Application', 4, new Date(2021, 1, 2), 'WIP'),
+    new Entry(3, 'Negotiate Project', 'Finalize Hours and get project go-ahead from Charles', 1, new Date(2020, 11, 12), 'DONE')
   ];
+
+  toggleDetails(index) {
+    this.entries[index].showDetails = !this.entries[index].showDetails;
+  }
 
   constructor() { }
 
