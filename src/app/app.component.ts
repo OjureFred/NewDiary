c20155cb8a4618134a9b6eb77bb8c12daad2a2e3
Fredrick Ojure <fredojure@hotmail.com>
2020-10-25 16:40:42 +0300

    Added code to reflect updated dairy entries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Entry } from './entry';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  entries: string[];
-  
-  constructor() {
-    this.entries = ["Watch Docker Tutorial", "Develop Simple Angular Application", "Finalize Hours and get project go-ahead from Charles"]
-  }
+
+  entries: Entry[] = [
+    { id: 1, description: 'Watch Docker Tutorial', date: new Date(2020, 10, 26)},
+    { id: 2, description: 'Develop Simple Angular Application', date: new Date(2021, 1, 2)},
+    { id: 3, description: 'Finalize Hours and get project go-ahead from Charles', date: new Date(2020, 11, 12)}
+  ]
+
 }
--- src/app/entry.ts
+++ src/app/entry.ts
@@ -1,2 +1,5 @@
 export class Entry {
+    id: number;
+    description: string;
+    date: Date;
 }
